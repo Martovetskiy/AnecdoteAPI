668e7cccfaae73a14a96716ae4adda3574d9e3cc
Stepan Melatonin <stepan.bodnar43@gmail.com>
2025-04-12 05:27:11 +0300

    refactor: delete unuse comments

--- AnecdoteAPI/Program.cs
+++ AnecdoteAPI/Program.cs
@@ -23,7 +23,6 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 
 app.MapGrpcService<AnecdoteServiceImpl>();
--- AnecdoteClient/AnecdoteClient.cs
+++ AnecdoteClient/AnecdoteClient.cs
@@ -1,10 +1,6 @@
 ﻿using Grpc.Net.Client;
 using AnecdoteAPI;
 using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 public class AnecdoteClientImpl
 {
